e35aed953152620941e7501bbf8ea522a0f67eac
parkdayoung <kei08032019@gmail.com>
2026-02-23 17:11:08 +0700

    Update index.js

--- Leetcode Problems/TwoSum/index.js
+++ Leetcode Problems/TwoSum/index.js
@@ -1,19 +1,11 @@
-function two_sum(numbers, target) {
-
-    for(let i = 0; i < numbers.length; i++) {
-        for(let j = i + 1; j < numbers.length; j++){
-            if (numbers[i] + numbers[j] === target) {
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
                 return [i, j];
             }
         }
     }
-
-    throw new Error("Nothing can found the target.")
-}
-
-
-
-console.log(two_sum([2, 7, 11, 15], 9)); // [0,1]
-console.log(two_sum([3, 2, 4], 6));      // [1,2]
-console.log(two_sum([3, 3], 6));         // [0,1]
-console.log(two_sum([3, 2, 19, 27], 42));
+    // Nếu không tìm thấy
+    throw new Error("No solution found");
+};
